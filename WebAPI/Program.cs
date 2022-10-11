a1e2e099a46dceb3b027066d9f7ebfb05d665e80
OktayHalitEkinay <67113059+OktayHalitEkinay@users.noreply.github.com>
2022-10-11 21:44:33 +0300

    Add project files.

---
+++ WebAPI/Program.cs
@@ -0,0 +1,43 @@
+using Autofac.Extensions.DependencyInjection;
+using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using Business.DependencyResolvers.Autofac;
+using Core.DependencyResolvers;
+using Core.Extensions;
+using Core.Utilities.IoC;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDependencyResolvers(new ICoreModule[] {
+               new CoreModule()
+            });
+builder.Services.AddCors();
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(builder => builder.RegisterModule(new AutofacBusinessModule()));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+          
